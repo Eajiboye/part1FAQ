c855cbaa986f5910c727e6138a107c4e1785b94b
emmanuela <eoa22@njit.edu>
2019-05-12 01:12:36 -0400

    Task:Added User profile migrate, model and updated the user seeder to create profile.

--- app/User.php
+++ app/User.php
@@ -1,18 +1,15 @@
 <?php
-
 namespace App;
-
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
 /**
  * App\User
  *
  * @property int $id
  * @property string $name
  * @property string $email
- * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $email_verified_at
  * @property string $password
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
@@ -34,16 +31,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-     'email', 'password',
+        'email', 'password',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -52,13 +47,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function profile()
+    {
+        return $this->hasOne('App\Profile');
+    }
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
 }
